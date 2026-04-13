ee02721694f7683a0f4b7d2f0dcf4ea9afdadc65
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-13 14:51:23 +0300

    deploy: v29 — emoji quiz, Türkçe sıfır

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1131305293" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3337522016" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
