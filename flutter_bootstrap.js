e1fd1e35869127f6738bbdff9b36cf03082df4fe
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-13 10:39:16 +0300

    deploy: v20 — Kurmancî TTS seslendirme

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3877070039" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1268963285" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
