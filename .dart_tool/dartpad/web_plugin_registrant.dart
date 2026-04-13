e02ecce3cf1e1a9762b091cbb8473cd1b9d22ec3
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-13 15:39:11 +0300

    deploy: v31 — quiz kelime gizleme düzeltmesi

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,38 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audio_session/audio_session_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:record_web/record_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioSessionWeb.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
