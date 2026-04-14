c28d9cebe328fd3a99a51550cae0e3857bb52767
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-14 08:13:22 +0300

    deploy: Lûtke Zarok kapsamlı iyileştirme

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "965491191" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1428000716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
