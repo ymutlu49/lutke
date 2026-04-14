3a0e124c8df4c01e49546a75a40fddbbabbafcdc
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-14 21:17:04 +0300

    deploy: 2026-04-14 21:17 - A1 heritage cumleler

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2988113913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "611398653" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
