a17b7fafc5324b4f3a8c8073e6bba4f5d3009d99
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-14 22:09:34 +0300

    deploy: 2026-04-14 22:09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3138522987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "140587469" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
