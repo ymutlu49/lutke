ec4fb93eec4a9437d4cc5333391272afb223c4f2
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-14 07:05:23 +0300

    deploy: Doğru Kürtçe durak adları + pedagojik sıralama

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2546760017" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3627522516" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
