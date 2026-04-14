42ce69b7c91694d9622ac87fbe216c6e1f36e604
nebezbextewar <y.mutlu@alparslan.edu.tr>
2026-04-14 06:50:09 +0300

    deploy: Quiz kökten yeniden yazıldı — anlamlı sorular

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3712336197" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2533092997" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
